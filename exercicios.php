c0ef41a27fa42b992150632ff2e51caf85e097e3
Adonis Cipriano <desenvolvimento777@gmail.com>
2021-05-31 01:41:22 -0300

    conversão dos arquivos para .php

---
+++ exercicios.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Curso de PHP 7 - Exercícios</title>
+</head>
+<body>
+
+    <header class="cabecalho">
+        <h1>Curso de PHP</h1>
+        <h2>Vizualização dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+    </main>
+    <footer class="rodape">
+        developed with &hearts; by Adonis Cipriano - <?= date('Y'); ?>
+    </footer>
+    
+</body>
+</html>
